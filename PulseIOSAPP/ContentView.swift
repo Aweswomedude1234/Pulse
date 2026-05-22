7a8537db31e1b08b0869b459cc9170f8da71aedf
Aweswomedude1234 <nithilan.murugesan64949@gmail.com>
2026-05-21 20:30:17 -0400

    Initial Commit

---
+++ PulseIOSAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulseIOSAPP
+//
+//  Created by Nithilan Murugesan on 5/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulseIOSAPP/PulseIOSAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseIOSAPPApp.swift
+//  PulseIOSAPP
+//
+//  Created by Nithilan Murugesan on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseIOSAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
